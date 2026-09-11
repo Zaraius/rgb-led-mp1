5d01a8868cf314ccf258c6c7e7d76426be769af6
Zaraius <zaraiusbillimoria@gmail.com>
2026-09-10 23:31:16 -0400

    fixed up code

--- rgb.sv
+++ rgb.sv
@@ -4,20 +4,21 @@ module top(
     input logic clk, 
     output logic RGB_R, RGB_G, RGB_B
 );
-    // Creating interval of 0.5s since clock is 12MHz
+    // Creating interval of 1/6seconds since clock is 12MHz
     parameter BLINK_INTERVAL = 2000000;
     // Creating a variable big enough to hold count up to blink interval, naming that variable count and setting it to 0
     logic [$clog2(BLINK_INTERVAL) - 1:0] count = 0;
-    logic [0:3] counter = 0;
-
+    // Counter for enumerating through colors
+    logic [2:0] counter = 0;
     logic colors [0:5][0:2] = '{
-        '{0, 1, 1},
-        '{0, 0, 1},
-        '{1, 0, 1},
-        '{1, 0, 0},
-        '{1, 1, 0},
-        '{0, 1, 0}};
-    // Starting with all of the Green and Blue off(1 is off in this case because its active low), and Red as on 
+        '{0, 1, 1}, // Red
+        '{0, 0, 1}, // Yellow
+        '{1, 0, 1}, // Green
+        '{1, 0, 0}, // Cyan
+        '{1, 1, 0}, // Blue
+        '{0, 1, 0}}; // Magenta
+
+    // Starting with all of the Red, Green and Blue off(1 is off in this case because its active low)
     initial begin
         RGB_R = 1'b1;
         RGB_G = 1'b1;
@@ -27,9 +28,11 @@ module top(
     always_ff @(posedge clk) begin
         if (count == BLINK_INTERVAL - 1) begin
             count <= 0;
+           // Setting led color based on 2d array and counter 
             RGB_R <= colors[counter][0];
             RGB_G <= colors[counter][1];
             RGB_B <= colors[counter][2];
+            // adding to counter and reseting it to 0 if above 6
             counter <= (counter + 1) % 6;
         end
         else begin
